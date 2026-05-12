7f8373dcf4480672eeffb8cbac99714ec780bf5e
mervekindan <mervekanmaz10@gmail.com>
2026-05-12 15:24:42 +0200

    Copy home page

--- space-galactica-app/src/app/page.js
+++ space-galactica-app/src/app/page.js
@@ -1,66 +1,32 @@
-import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+const HeroSection = () => {
+    return (
+        <main className={styles.hero}>
+            <div className={styles.heroContent}>
+                <p className={styles.heroH5}>SO, YOU WANT TO TRAVEL TO</p>
+                <p className={styles.heroJumbo}>SPACE</p>
+                <p className={styles.heroContent}>
+                    Let’s face it; if you want to go to space, you might as well
+                    genuinely go to outer space and not hover kind of on the
+                    edge of it. Well sit back, and relax because we’ll give you
+                    a truly out of this world experience!
+                </p>
+            </div>
+            <Link href="/about_us" className={styles.exploreButton}>
+                EXPLORE
+            </Link>
+        </main>
+    );
+};
+
+export const Home = () => {
+    return (
+        <div className={styles.app}>
+            <HeroSection />
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}
+    );
+};
+
+export default Home;
